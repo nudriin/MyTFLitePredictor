df61a8d1be9a526a36dbb6b702024d560212104d
Nurdin <mrsunnysummer@gmail.com>
2024-11-08 04:47:01 +0700

    feat: integrate the model with main activity

--- app/src/main/java/com/nudriin/mytflitepredictor/MainActivity.kt
+++ app/src/main/java/com/nudriin/mytflitepredictor/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.nudriin.mytflitepredictor
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.nudriin.mytflitepredictor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var predictorHelper: PredictorHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        predictorHelper = PredictorHelper(
+            context = this,
+            onResult = {result ->
+                binding.tvResult.text = result
+            },
+            onError = {errorMessage ->
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        binding.btnPredict.setOnClickListener {
+            val input = binding.edSales.text.toString()
+            predictorHelper.predict(input)
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        predictorHelper.close()
+    }
+
+
 }
